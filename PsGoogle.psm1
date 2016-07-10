b8350aec2a34d92b9d342097a13b511d498f0c78
Gordon Cowie <gcowie@live.com>
2016-07-10 00:50:10 -0700

    workaround for missing web search api #1

--- PsGoogle.psm1
+++ PsGoogle.psm1
@@ -9,26 +9,48 @@ function google([Parameter(ValueFromRemainingArguments=$true)][string]$s) {
         }
         write-host
     }
-    $page = 0
-    $pages = @(@{start=0})
-    while ($page -lt $pages.Count) {
-        $c = irm "https://ajax.googleapis.com/ajax/services/search/web?v=1.0&q=$([Web.HttpUtility]::UrlEncode($s))&start=$($pages[$page].start)"
-        if ($c.responseDetails) { return $c.responseDetails }
-        if ($c.responseData.results.Count -eq 0) { return "no results." }
-        $pages = $c.responseData.cursor.pages
-        write-host -foreground yellow "`r`nShowing $(1 + $pages[$page].start) to $($c.responseData.results.Count + $pages[$page].start) of $($c.responseData.cursor.resultCount)`r`n"
-        $c.responseData.results | % {
-            write-bold ([Web.HttpUtility]::HtmlDecode($_.title))
-            write-host -foreground darkcyan $_.unescapedUrl
-            write-bold ([Web.HttpUtility]::HtmlDecode($_.content))
-            write-host
-        }
-        if (($page = 1 + $page) -lt $pages.Count) {
-            write-host "any key for more results, q to quit.."
-            if ([Console]::ReadKey($true).Key -eq "q") { break }
+
+    $page = 0; $c = 1
+    while ($true) {
+        $doc = (iwr "https://www.google.com/search?q=$([Web.HttpUtility]::UrlEncode($s))&start=$(($page++) * 10)").ParsedHtml
+        $stats = $doc.getElementById("resultStats").innerHTML
+        if (!$stats -and $c -gt 1) { break } elseif (!$stats) { write-host -foreground red "`nno results.`n"; break }
+        write-host -foreground yellow `n$stats`n
+
+        # workaround/speedhack since $doc.getElementsByClassName('g') doesn't work and piping everything back is terribly slow..
+        $js = "
+            var g = document.getElementsByTagName('div');
+            for(var i = 0; i < g.length; i++) {
+                if(g[i].className == 'g') {
+                    var r = document.createElement('psgoogle');
+                    try { r.setAttribute('url', g[i].childNodes[0].childNodes[0].href) } catch(e) {}
+                    try { r.setAttribute('text', g[i].childNodes[0].childNodes[0].innerText) } catch(e) {}
+                    try { r.setAttribute('title', g[i].childNodes[0].childNodes[0].innerHTML) } catch(e) {}
+                    try { r.setAttribute('desc', g[i].childNodes[1].childNodes[1].innerHTML) } catch(e) {}
+                    document.appendChild(r);
+                }
+            }"
+        $doc.parentWindow.execScript($js, "javascript")
+        $doc.getElementsByTagName("psgoogle") | % {
+            $url = $_.getAttribute('url', 1)
+            if (!$url) {
+                write-host $_.getAttribute('text', 1)
+                write-host
+            } elseif ($url -ne [DBNull]::Value) {
+                write-bold "$(($c++)). $($_.getAttribute('title', 1))"
+                write-host -foreground darkcyan ([Web.HttpUtility]::UrlDecode($url.Substring(13, $url.IndexOf("&sa=") - 13)))
+                write-bold ([Web.HttpUtility]::HtmlDecode($_.getAttribute('desc', 1)).Replace("<BR>", "`n"))
+                write-host
+            }
+
+            if ($c % 5 -eq 0) {
+                write-host "any key for more results, q to quit.."
+                if ([Console]::ReadKey($true).Key -eq "q") { break }
+            }
         }
     }
 }
+
 
 if (gcm Register-ArgumentCompleter -ea Ignore) {
     Register-ArgumentCompleter -Command 'google' -Parameter 's' -Script {
