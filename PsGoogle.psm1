23fe1c2fe8abf3e14d1f7f4cd6dd2872f1d72809
Gordon Cowie <gcowie@live.com>
2016-07-11 00:06:15 -0700

    ditch IHTMLDocument and just parse results directly, add -o switch for scripting

--- PsGoogle.psm1
+++ PsGoogle.psm1
@@ -1,7 +1,18 @@
 Add-Type -AssemblyName System.Web
 Add-Type -AssemblyName System.Management.Automation
 
-function google([Parameter(ValueFromRemainingArguments=$true)][string]$s) {
+function google([Parameter(Position=0,Mandatory=$true,ValueFromRemainingArguments=$true)][string]$q, [int]$o) {
+    function parse-results([string]$html) {
+        $reg = new-object Text.RegularExpressions.Regex('class="r"><a href="/url\?q=(.*?)&amp;sa=.*?>(.*?)</a.*?<span class="st">(.*?)</span',
+            @([Text.RegularExpressions.RegexOptions]::IgnoreCase, [Text.RegularExpressions.RegexOptions]::Singleline, [Text.RegularExpressions.RegexOptions]::Compiled))
+        $reg.Matches($html) | % {
+            new-object PSObject -prop @{
+                'url' = [Web.HttpUtility]::UrlDecode([Web.HttpUtility]::UrlDecode($_.groups[1].value))
+                'title' = [Web.HttpUtility]::HtmlDecode($_.groups[2].value)
+                'summary' = [Web.HttpUtility]::HtmlDecode(($_.groups[3].value -replace '(?!</?b>)<.*?>', '')).Replace("`n", "")
+            }
+        }
+    }
     function write-bold([string]$s) {
         $i = (-not $s.StartsWith("<b>"))
         $s -split "<b>|</b>" | ? { $_ -ne "" } | % {
@@ -10,42 +21,28 @@ function google([Parameter(ValueFromRemainingArguments=$true)][string]$s) {
         write-host
     }
 
-    $page = 0; $c = 1
-    while ($true) {
-        $doc = (iwr "https://www.google.com/search?q=$([Web.HttpUtility]::UrlEncode($s))&start=$(($page++) * 10)").ParsedHtml
-        $stats = $doc.getElementById("resultStats").innerHTML
-        if (!$stats -and $c -gt 1) { break } elseif (!$stats) { write-host -foreground red "`nno results.`n"; break }
-        write-host -foreground yellow `n$stats`n
+    $page = 0; $i = 1
+    while (!$o -or $o -gt $page) {
+        $raw = irm "https://www.google.com/search?q=$([Web.HttpUtility]::UrlEncode($q))&start=$(($page++)*10)"
+        if ($o) { parse-results $raw } else {
+            $stats = ([regex]'id="resultStats">(.*?)<').match($raw)
+            if (!$stats.success -and $c -gt 1) { break } elseif (!$stats.success) { write-host -foreground red "`nno results.`n"; break }
+            write-host -foreground yellow "`n$($stats.groups[1].value)`n"
 
-        # workaround/speedhack since $doc.getElementsByClassName('g') doesn't work and piping everything back is terribly slow..
-        $js = "
-            var g = document.getElementsByTagName('div');
-            for(var i = 0; i < g.length; i++) {
-                if(g[i].className == 'g') {
-                    var r = document.createElement('psgoogle');
-                    try { r.setAttribute('url', g[i].childNodes[0].childNodes[0].href) } catch(e) {}
-                    try { r.setAttribute('text', g[i].childNodes[0].childNodes[0].innerText) } catch(e) {}
-                    try { r.setAttribute('title', g[i].childNodes[0].childNodes[0].innerHTML) } catch(e) {}
-                    try { r.setAttribute('desc', g[i].childNodes[1].childNodes[1].innerHTML) } catch(e) {}
-                    document.appendChild(r);
+            $info = ([regex]'id="topstuff">(?:<(?!h3)[^>]*?>)+(?![<[])(.+?)</div>').match($raw)
+            if ($info.success) { write-host -foreground green ([Web.HttpUtility]::HtmlDecode(($info.groups[1].value -replace '<.*?>', ' ' -replace ' {2,}', ' ')).Trim()) }
+            write-host
+
+            parse-results $raw | % {
+                write-bold "$(($i++)). $($_.title)"
+                write-host -foreground darkcyan $_.url
+                write-bold $_.summary
+                write-host
+                if ($i % 5 -eq 0) {
+                    write-host "any key for more results, q to quit.."
+                    if ([Console]::ReadKey($true).Key -eq "q") { break }
+                    write-host
                 }
-            }"
-        $doc.parentWindow.execScript($js, "javascript")
-        $doc.getElementsByTagName("psgoogle") | % {
-            $url = $_.getAttribute('url', 1)
-            if (!$url) {
-                write-host $_.getAttribute('text', 1)
-                write-host
-            } elseif ($url -ne [DBNull]::Value) {
-                write-bold "$(($c++)). $($_.getAttribute('title', 1))"
-                write-host -foreground darkcyan ([Web.HttpUtility]::UrlDecode($url.Substring(13, $url.IndexOf("&sa=") - 13)))
-                write-bold ([Web.HttpUtility]::HtmlDecode($_.getAttribute('desc', 1)).Replace("<BR>", "`n"))
-                write-host
-            }
-
-            if ($c % 5 -eq 0) {
-                write-host "any key for more results, q to quit.."
-                if ([Console]::ReadKey($true).Key -eq "q") { break }
             }
         }
     }
@@ -53,7 +50,7 @@ function google([Parameter(ValueFromRemainingArguments=$true)][string]$s) {
 
 
 if (gcm Register-ArgumentCompleter -ea Ignore) {
-    Register-ArgumentCompleter -Command 'google' -Parameter 's' -Script {
+    Register-ArgumentCompleter -Command 'google' -Parameter 'q' -Script {
         param($commandName, $parameterName, $wordToComplete, $commandAst, $fakeBoundParameter)
         (irm "https://suggestqueries.google.com/complete/search?output=xml&q=$([Web.HttpUtility]::UrlEncode($wordToComplete))").SelectNodes("/toplevel/CompleteSuggestion/*") | % {
             new-object Management.Automation.CompletionResult($_.data)
